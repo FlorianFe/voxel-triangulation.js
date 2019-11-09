8c4eca1abcb5e8d8e7a3048fb67390fcb4386b72
Florian Fechner <florian@Florians-MBP.fritz.box>
2019-11-09 18:26:58 +0100

    🐞bug fix

--- src/binarify/binarify.js
+++ src/binarify/binarify.js
@@ -1,6 +1,19 @@
 
 const zeros = require('zeros');
 
+const isBorder = (x, y, z, sx, sy, sz) => 
+{
+    if(x === 0) return true;
+    if(y === 0) return true;
+    if(z === 0) return true;
+
+    if(x === sx - 1) return true;
+    if(y === sy - 1) return true;
+    if(z === sz - 1) return true;
+
+    return false;
+}
+
 const binarify = (voxelSpace, exclude) =>
 {
     const SOLID = 1;
@@ -15,7 +28,7 @@ const binarify = (voxelSpace, exclude) =>
     {
         let value = voxelSpace.get(x, y, z);
 
-        if(exclude.includes(value)) 
+        if(exclude.includes(value) || isBorder(x, y, z, sx, sy, sz)) 
         {
             result.set(x, y, z, NOT_SOLID);
         }
